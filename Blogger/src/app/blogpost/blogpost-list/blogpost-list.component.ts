e0edb3c37db058d2dac0418d71ab4924b6681812
kishorechintucls <kishore.p29@gmail.com>
2020-01-25 20:00:46 +0530

    Added Service Blog

---
+++ Blogger/src/app/blogpost/blogpost-list/blogpost-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from "@angular/core";
+import { BlogpostService } from "../blogpost.service";
+import { Blogpost } from "../blogpost";
+import { Title } from "@angular/platform-browser";
+
+@Component({
+  selector: "app-blogpost-list",
+  templateUrl: "./blogpost-list.component.html",
+  styleUrls: ["./blogpost-list.component.css"]
+})
+export class BlogpostListComponent implements OnInit {
+  title = "Blogs";
+  blogs: Blogpost;
+  error: {};
+
+  constructor(
+    private titleService: Title,
+    private blogPostService: BlogpostService
+  ) {}
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.blogPostService.getBlogs().subscribe(
+      (data: Blogpost) => {
+        this.blogs = data;
+      },
+      error => (this.error = error)
+    );
+  }
+}
--- Blogger/src/environments/environment.ts
+++ Blogger/src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+         production: false,
+         baseurl: "http://localhost/demoproject/blogger/"
+       };
 
 /*
  * For easier debugging in development mode, you can import the following file
